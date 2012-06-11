8cec3d5c7abe0347036d1dd058a981138feeee69
Chris Jaure <chrisjaure@gmail.com>
2012-06-11 09:38:38 -0500

    add tile to Item, remove unneeded dependency

--- examples/circles.js
+++ examples/circles.js
@@ -1,8 +1,7 @@
 var texture = new canvas.Raster('/home/chris/Downloads/bgnoise_lg.png');
 texture.blendMode = 'multiply';
 texture.opacity = 0.5;
-canvas.tile(texture);
-
+texture.tile();
 /*
 new Raster({
 	file: '/home/chris/Downloads/bgnoise_lg.png',
@@ -19,19 +18,6 @@ circle.style = {
 };
 circle.opacity = 0.2;
 circle.blendMode = 'overlay';
-
-canvas.tile(circle, function(item, i) {
-
-	if (i % 14 === 0) {
-		item.style.fillColor = 'yellow';
-	}
-
-	if (i % 6 === 0 || i % 5 === 0) {
-		item.remove();
-	}
-
-});
-
 /*
 var circle = new Circle({
 	point: [0,0],
@@ -41,20 +27,14 @@ var circle = new Circle({
 	strokeWidth: 2,
 	opacity: 0.2,
 	blendMode: 'overlay'
-}).tile()
-	.nthChild(14, function() {
-		this.fillColor = 'yellow';
-	})
-	.nthChild(6, function() {
-		this.remove();
-	});
+})
 */
 
 var circle2 = circle.clone();
 circle2.scale(0.5);
 circle2.style.fillColor = 'blue';
 
-canvas.tile(circle2, function(item, i) {
+circle2.tile(function(item, i) {
 
 	if (i % 15 === 0) {
 		item.style.fillColor = 'yellow';
@@ -65,7 +45,6 @@ canvas.tile(circle2, function(item, i) {
 	}
 
 });
-
 /*
 circle.clone({
 	scale: 0.5,
@@ -83,12 +62,11 @@ var circle3 = circle.clone();
 circle3.scale(3);
 circle3.style.fillColor = 'orange';
 
-canvas.tile(circle3, function(item, i) {
+circle3.tile(function(item, i) {
 
 	item.opacity = Math.min(Math.random(), 0.3);
 
 });
-
 /*
 circle.clone({
 	scale: 0.3,
@@ -96,6 +74,27 @@ circle.clone({
 }).tile(function() {
 	this.opacity = Math.min(Math.random(), 0.3);
 });
+*/
+
+circle.tile(function(item, i) {
+
+	if (i % 14 === 0) {
+		item.style.fillColor = 'yellow';
+	}
+
+	if (i % 6 === 0 || i % 5 === 0) {
+		item.remove();
+	}
+
+});
+/*
+circle.tile()
+	.nthChild(14, function() {
+		this.fillColor = 'yellow';
+	})
+	.nthChild(6, function() {
+		this.remove();
+	});
 */
 
 canvas.setBackgroundColor('pink');
